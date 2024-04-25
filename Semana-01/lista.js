81a17d419fb67f4668f20ac738ef169506672d9c
Diego-Lucena <diego_silva_lucena@hotmail.com>
2024-04-24 22:55:01 -0300

    Tarefas essenciais

---
+++ Semana-01/lista.js
@@ -0,0 +1,13 @@
+let nome = "Diego";
+let idade = 30;
+let cpf = "444.444.444-44"
+let altura = 1.80;
+let maiorIdade = true;
+
+let arrPessoas = [nome, idade, cpf, altura, maiorIdade];
+
+
+for (i = 0; i < arrPessoas.length; i++) {
+
+    console.log(arrPessoas[i]);
+}
