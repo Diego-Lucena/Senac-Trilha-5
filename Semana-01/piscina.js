e77bd392aa861e3a3d0344d6014a0ef214ff75d8
Diego-Lucena <166460557+Diego-Lucena@users.noreply.github.com>
2024-07-02 20:51:12 -0300

    Delete Semana-01 directory

--- Semana-01/piscina.js
+++
@@ -1,8 +0,0 @@
-const raio = 3.5; 
-const altura = 1.6; 
-
-let areaBase = Math.PI * Math.pow(raio, 2);
-let cubico = areaBase * altura;
-let litros = cubico * 1000; 
-
-console.log("O volume da piscina é: " + litros + " litros");
